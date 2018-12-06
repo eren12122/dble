af63bcd979c20939236b0172b8dbc7051bdbaebd
eren12122 <44553580+eren12122@users.noreply.github.com>
2018-12-06 19:20:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,4 +103,21 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
+const Discord = require("discord.js");
+const Cleverbot = require("cleverbot-node");
+const client = new Discord.Client();
+const clbot = new Cleverbot;
+ 
+client.on("message", message => {
+  if (message.channel.type === "dm") {
+    clbot.write(message.content, (response) => {
+      message.channel.startTyping();
+      setTimeout(() => {
+        message.channel.send(response.output).catch(console.error);
+        message.channel.stopTyping();
+      }, Math.random() * (1 - 3) + 1 * 1000);
+    });
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
