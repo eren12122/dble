33a44db5e979552239e4d5bf0c02ae98390da1d2
eren12122 <44553580+eren12122@users.noreply.github.com>
2018-12-06 19:22:30 +0300

    Update canlidestek.js

--- komutlar/canlidestek.js
+++ komutlar/canlidestek.js
@@ -1,31 +1,16 @@
-const Discord = require('discord.js');
-
-exports.run = async (client, message, params) => {
-  var channel = client.channels.find('id', '519217765114970124')
-    const asdf = await client.channels.get(message.channel.id).createInvite()
-  message.delete();
-  const embed = new Discord.RichEmbed()
-  .setTitle("» Pop Bot | Canlı Destek")
-  .setDescription("**Canlı Destek İsteğin Ekibimize Bildirildi Lütfen Bekleyiniz**")
-  .setFooter("Eğer Uzun Süre Boyunca Yanıt Almazsanız owner ile iletişime geçin! ")
- message.channel.send(embed)
-      const invite = new Discord.RichEmbed()
-  .setAuthor("» Canlı Destek | Talep")
-  .addField('**» Kullanıcı Adı: **', message.author.username + '#' + message.author.discriminator)
-  .addField('**» Sunucu Adı: **', message.guild.name)
-  .setDescription(asdf.url)
-      channel.send(invite)
-};
-  
-  exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'canlıdestek',
-  description: 'Canlı Destek Tablebi Oluşturur.',
-  usage: 'canlıdestek'
-};
+const Discord = require("discord.js");
+const Cleverbot = require("cleverbot-node");
+const client = new Discord.Client();
+const clbot = new Cleverbot;
+ 
+client.on("message", message => {
+  if (message.channel.type === "dm") {
+    clbot.write(message.content, (response) => {
+      message.channel.startTyping();
+      setTimeout(() => {
+        message.channel.send(response.output).catch(console.error);
+        message.channel.stopTyping();
+      }, Math.random() * (1 - 3) + 1 * 1000);
+    });
+  }
+});
